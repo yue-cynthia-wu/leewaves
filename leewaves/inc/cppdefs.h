ce439068934d261818a0e60ee16d2436ebcc42e1
Yue Cynthia Wu <45446700+yue-cynthia-wu@users.noreply.github.com>
2020-10-07 11:28:43 -0400

    HPC

---
+++ leewaves/inc/cppdefs.h
@@ -0,0 +1,16 @@
+#undef   runtracmass
+#define  periodic_ew
+#undef   allow_particle
+!#undef   rhoonly
+#define   rhoonly
+
+#undef   relaxation
+#define  fixed_bottom_thickness
+#undef   gotm_call
+#define  file_output
+#define  file_output_cdf
+#undef   file_output_bin
+#undef   implicit
+   
+#undef  sigma_stretch
+!#define  sigma_stretch
